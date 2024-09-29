3ff59a1af33b26d3923ffa9fd1a5af5b77fc8726
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-09-29 19:16:11 -0300

    v2.1

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,15 +28,14 @@ export default function Header() {
     <header className={`${isScrolled ? 'bg-white' : 'bg-white shadow-md'} fixed top-0 left-0 w-full z-50 transition-colors duration-300 py-4`}>
       <div className="container mx-auto flex justify-between items-center">
         <div className="logo">
-          {/* Logo */}
-          <Image 
-            src="/logo-web.png" 
-            alt="Path Logo" 
-            width={180} 
-            height={80} 
-            className="w-32"
-          />
-        </div>
+  <Image 
+    src="/logo-web.png" 
+    alt="Path Logo" 
+    width={isMobileMenuOpen ? 120 : 180}  // Más pequeño en móviles
+    height={isMobileMenuOpen ? 60 : 80}
+    className="w-32 md:w-48"  // Ajuste para dispositivos móviles
+  />
+</div>
         {/* Desktop Menu */}
         <nav className="hidden md:flex space-x-6">
           <Link href="/" className={`text-primary hover:text-secondary transition-colors duration-300`}>
